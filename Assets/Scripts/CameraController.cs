1513ab36031e44f72b83fa9123fdef279e4da858
kamiyama <chidayamika02@gmail.com>
2023-09-30 13:56:02 +0900

    ファイルを復元

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class CameraController : MonoBehaviour
+{
+    public int score=0;
+    Vector3 angle;
+    Vector3 primaryAngle;
+    float yLimit = 40f;
+    float xLimit = 25f;
+    public float sensitivity = 1;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        angle = this.gameObject.transform.localEulerAngles;
+        primaryAngle = this.gameObject.transform.localEulerAngles;
+    }
+    void Update()
+    {
+        angle.y += Input.GetAxis("Mouse X") * sensitivity;
+
+        if (angle.y <= primaryAngle.y - yLimit)
+        {
+            angle.y = primaryAngle.y - yLimit;
+        }
+        if (angle.y >= primaryAngle.y + yLimit)
+        {
+            angle.y = primaryAngle.y + yLimit;
+        }
+
+        angle.x -= Input.GetAxis("Mouse Y") * sensitivity;
+
+        if (angle.x <= primaryAngle.x - xLimit)
+        {
+            angle.x = primaryAngle.x - xLimit;
+        }
+        if (angle.x >= primaryAngle.x + xLimit)
+        {
+            angle.x = primaryAngle.x + xLimit;
+        }
+
+        this.gameObject.transform.localEulerAngles = angle;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                Destroy(hit.collider.gameObject);
+                score++;
+                Debug.Log(score);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TargetGenerator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetGenerator : MonoBehaviour
+{
+    public GameObject target;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GameObject targetObj = GameObject.Find(target.name);
+        if (targetObj == null)
+        {
+            float x = Random.Range(-7f, 7f);
+            float y = Random.Range(-4f, 4f);
+            float z = Random.Range(-1f, 5f);
+            GameObject newTargetObj = Instantiate(target, new Vector3(x, y, z), target.transform.rotation);
+            newTargetObj.name = target.name;
+        }
+
+    }
+    
+}
