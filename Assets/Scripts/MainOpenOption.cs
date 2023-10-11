37b23da090eb34a07a453e920340dabea83a7930
maeyama <Cralia.kzhr@gmail.com>
2023-10-11 14:44:06 +0900

    optionUI調整3

---
+++ Assets/Scripts/MainOpenOption.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainOpenOption : MonoBehaviour
+{
+    public GameObject option;
+    public CameraController camCon;
+    // Update is called once per frame
+    public void Update(){
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            option.SetActive(true);
+            camCon.GetComponent<CameraController>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+          
+        }
+    }
+}
